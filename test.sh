6d751f4d16b4cb5e41c2111fe895cf147eb62f70
stoneyen <stone.yen@gmail.com>
2022-06-11 16:15:08 +0800

    test commit

---
+++ test.sh
@@ -0,0 +1,20 @@
+curl -d '{
+  "_links": {
+    "self": {
+      "href": "https://foo.com"
+    }
+  },
+  "arena": {
+    "dims": [4,3],
+    "state": {
+      "https://foo.com": {
+        "x": 0,
+        "y": 0,
+        "direction": "N",
+        "wasHit": false,
+        "score": 0
+      }
+    }
+  }
+}' -H "Content-Type: application/json" -X POST -w "\n" \
+  <THE URL YOU COPIED>
